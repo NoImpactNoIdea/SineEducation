cdbd5254d1bc2b1bc26095080de9d8b7cd9860da
NoImpactNoIdea <noimpactapplications@gmail.com>
2024-05-04 15:36:17 -0600

    Initial Commit

---
+++ SineEducation/SineEducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SineEducationApp.swift
+//  SineEducation
+//
+//  Created by Cristina Arcodia on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SineEducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
